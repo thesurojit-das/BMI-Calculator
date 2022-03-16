493d94b2207d68c31b29dbc83d47d77e22361e33
thesurojit-das <2005487@kiit.ac.in>
2022-03-16 10:55:53 +0530

    BMI Calculator

---
+++ bmi.js
@@ -0,0 +1,37 @@
+function fun(x)
+    {
+        return Number.parseFloat(x).toFixed(1);
+    }
+    
+function cal()
+    {
+        var x=document.getElementById("height").value;
+        var y=document.getElementById("weight").value;
+        var a=x*x;
+        var b=y/a*10000;
+        document.getElementById("bmi").value=fun(b);
+        event.preventDefault();
+        if(b<16)
+            document.getElementById("remark").innerHTML="Severe Thinness"   
+        else if (b>=16   &&b<17){
+            document.getElementById("remark").innerHTML="Moderate Thinness"   
+        } 
+        else if (b>=17  &&b<18.5){
+            document.getElementById("remark").innerHTML="Mild Thinness"    
+        }
+        else if (b>=18.5 &&b<25){
+            document.getElementById("remark").innerHTML="Normal"    
+        }
+        else if (b>=25  &&b<30){
+            document.getElementById("remark").innerHTML="Overweight"    
+        } 
+        else if (b>30  &&b<35){
+            document.getElementById("remark").innerHTML="Obese Class I"    
+        }
+        else if (b>=35  &&b<40){
+            document.getElementById("remark").innerHTML="Obese Class II"    
+        }
+        else{
+            document.getElementById("remark").innerHTML="Obese Class III" 
+        } 
+}
